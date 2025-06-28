543a9d268e26abde5aec32731ddb9b407da97edb
Guido Varga <guidovargamartinez123@gmail.com>
2025-06-28 19:12:48 -0300

    db build

---
+++ packages/db/prisma/migrations/20250628220935_init/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Business" ADD COLUMN     "closedOn" TEXT,
+ADD COLUMN     "googlePlaceId" TEXT,
+ADD COLUMN     "latitude" DOUBLE PRECISION,
+ADD COLUMN     "longitude" DOUBLE PRECISION;
