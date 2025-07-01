717694075d9382013eaaef89901e41b3acec72c5
Guido Varga <guidovargamartinez123@gmail.com>
2025-06-30 21:01:18 -0300

    test config experimental

--- apps/mechanic-finder/next.config.js
+++ apps/mechanic-finder/next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
     serverSourceMaps: true,
   },
   reactStrictMode: false,
-  outputFileTracingIncludes: {
-    '/*': ['node_modules/@rubros/db/dist/generated/prisma/**/*'],
-  },
 };
 
 export default nextConfig;
